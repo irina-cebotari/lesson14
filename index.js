05f9fb296cbfd4bb9822a25a202c71426d3a91b9
irina-cebotari <irina.cebotari0301@gmail.com>
2024-03-10 20:28:09 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,133 @@
+// const condition = true
+
+// if(condition) {
+//     console.log('instructiuni')
+// }
+
+
+// const str = 'string'
+
+// switch (str) {
+//     case 'str':
+//         console.log('actiuni')
+// }
+
+// const obj = {
+//     menu1: 'menu 1',
+//     menu2: 'menu 2',
+//     menu3: 'menu 3',
+//     menu4: 'menu 4',
+
+// }
+
+// console.log(obj)
+// console.log('string')
+// console.log([])
+
+// console.log(Object.keys(obj))
+// console.log(Object.values(obj))
+
+// console.log(obj.menu1)
+// console.log(obj['menu 1'])
+
+
+// const str = 'string'
+
+// const obj1 = {
+//     menu: 'menu1',
+
+//     getMenu: () => console.log('menu1')
+// }
+
+// obj1.getMenu()
+
+
+// console.log(str.toUpperCase())
+// console.log(str.toLowerCase())
+
+
+// const func = (a, b) => {
+//     console.log(a + b);
+// }
+
+// func(2, 3)
+// func(4, 6)
+// func(-1, 7)
+
+
+// const func1 = (a, b) => {
+//     return a+b;
+// }
+    
+// console.log(func1(2,3))
+// console.log(func1(1,3))
+// console.log(func1(2,3))
+
+
+// const str = 'string'
+
+// console.log(str.charAt(0))
+
+// const func1 = (index, str) => {
+//     return str[index]
+// }
+
+// console.log(func1(0, str))
+
+// console.log(str.length)
+// console.log(str.charAt(str.length - 1))
+
+
+// const str = 'here we have some text'
+
+// console.log(str.substring(0, 4))
+
+// console.log(str.slice(-10))
+
+// console.log(str.indexOf('e'))
+
+// console.log(str.lastIndexOf('e'))
+
+// console.log(str.replace('we', 'you'))
+// console.log(str.replaceAll('we', 'you'))
+
+// console.log(str.split(' '))
+
+// console.log(11%3)
+
+// const num = 5.6666666
+
+
+// console.log(num.toFixed(2))
+
+// console.log(0.2 + 0.1)
+
+// const num1 = 0.2
+// const num2 = 0.1
+
+// const sum = ((num1 * 10) + (num2 * 10))/10
+
+// console.log(sum)
+
+// console.log(10000000000000000001 + 10000000000000000001)
+
+
+// console.log(Math.floor(num))
+// console.log(Math.sqrt(num))
+// console.log(Math.random() * 10)
+// console.log(Math.floor(Math.random() * 10))
+
+
+
+// const func = (a, b, c) => {
+//     return (a + b - c)
+// }
+    
+// console.log(func(10, 5, 4))
+
+
+// const func1 = (str) => {
+//     return `first letter: ${str.charAt(0)}, last letter: ${str.charAt(str.length - 1)}`
+// }
+
+// console.log(func1('string'))
